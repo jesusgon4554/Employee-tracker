547d768b15bd772fd606c34be68462f7d59b0984
jesusgon4554 <jesus.gonzalez.jg735@gmail.com>
2020-10-29 10:28:29 -0700

    added departments and inquirers

--- script.js
+++ script.js
@@ -9,38 +9,12 @@ var connection = mysql.createConnection({
     //username
     user: "root",
     //password
-    password: "",
+    password: "Sprinkles54",
     //database name
     database: "employeeTracker_db"
 });
 
-function addDepartment(){
-    //add department "name" to database
-}
 
-function addRole(){
-    //add role "title", "salary", and its department_id
-}
-
-function addEmployee(){
-    //add employee first_name, last_name, role_id, manager_id
-}
-
-function viewDep(){
-    //display the departments table in the database
-}
-
-function viewRoles(){
-    //display the role table
-}
-
-function viewEmployees(){
-    //display the employees table
-}
-
-function updateRoles(){
-    // take employee role_id and change it 
-}
 
 function displayMenu(){
     inquirer.prompt([
@@ -57,9 +31,9 @@ function displayMenu(){
             "View Employees",
             "Update Employee roles"
         ]
-        }
-    ]).then(answer =>{
-        let choice = answer.chosenActivity;
+    }
+]).then(answer =>{
+    let choice = answer.chosenActivity;
         switch(choice){
             case "Add a Department":
                 return addDepartment();
@@ -79,9 +53,74 @@ function displayMenu(){
     })
 }
 
+function addDepartment(){
+    //add department "name" to database
+    let departments = [];
+
+    inquirer.prompt([
+    {
+        type:"list",
+        message: "Order by:",
+        choices: [
+            "ID",
+            "Name"
+        ],
+        name:"orderChoice"
+    }
+]).then(answer => {
+    const { orderChoice } = answer
+    let query = ""; 
+
+    if(orderChoice === "ID") {
+        query = "SELECT department.* from department ORDER BY department.id";
+    } else if(orderChoice === "Name") {
+        query = "SELECT department.* from department ORDER BY department.name";
+    }
+    connection.query(query, function(err, res) {
+        if(err) displayError(); 
+
+        //For each department, store the ID and name. 
+        res.forEach((department, key, departmentArray) => {
+            departments.push({
+                "ID": department.id,
+                "Name": department.name
+            }); 
+        })
+    });
+});
+
+// function addRole(){
+//     //add role "title", "salary", and its department_id
+// }
+
+// function addEmployee(){
+//     //add employee first_name, last_name, role_id, manager_id
+// }
+
+// function viewDep(){
+//     //display the departments table in the database
+// }
+
+// function viewRoles(){
+//     //display the role table
+// }
+
+// function viewEmployees(){
+//     //display the employees table
+// }
+
+// function updateRoles(){
+//     // take employee role_id and change it 
+// }
+
+
+
 connection.connect(function(err){
     if(err) throw err;
     console.log(`connected as id ${connection.threadID}`);
-    displayMenu();
+    
     connection.end();
-})
+    })
+}
+
+displayMenu();
